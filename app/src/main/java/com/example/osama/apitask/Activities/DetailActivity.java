b03f0eb01e91156fd1f2627092667ff557d0539d
Osama <asimo@uni.coventry.ac.uk>
2019-12-10 13:59:15 +0300

    Details activity added

--- app/src/main/java/com/example/osama/apitask/Activities/DetailActivity.java
+++ app/src/main/java/com/example/osama/apitask/Activities/DetailActivity.java
@@ -1,13 +1,15 @@
-package com.example.osama.apitask;
+package com.example.osama.apitask.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.osama.apitask.R;
+
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
     }
 }
---
+++ app/src/main/java/com/example/osama/apitask/ArticleApplication.java
@@ -0,0 +1,4 @@
+package com.example.osama.apitask;
+
+public class ArticleApplication {
+}
