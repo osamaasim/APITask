6a1558c06d7eb983fd1ba9d93efb3ad186ee013e
Osama <asimo@uni.coventry.ac.uk>
2019-12-10 23:07:30 +0300

    Model layer implemented

---
+++ app/src/main/java/com/example/osama/apitask/Model/News.java
@@ -0,0 +1,45 @@
+package com.example.osama.apitask.Model;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.example.osama.apitask.BR;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class News extends BaseObservable implements Serializable {
+    @SerializedName("title")
+    @Bindable
+    private final String title;
+
+    @SerializedName("byline")
+    @Bindable
+    private final String byline;
+
+    @SerializedName("published_date")
+    @Bindable
+    private final String published_date;
+
+    public News(String byline, String title, String published_date) {
+        this.title = title;
+        this.byline = byline;
+        this.published_date = published_date;
+
+        notifyPropertyChanged(BR.title);
+        notifyPropertyChanged(BR.byline);
+        notifyPropertyChanged(BR.published_date);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getByline() {
+        return byline;
+    }
+
+    public String getPublished_date() {
+        return published_date;
+    }
+}
